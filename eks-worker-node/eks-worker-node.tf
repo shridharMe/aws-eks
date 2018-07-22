4b494aa4c4329e474204efe6f87dc4de8ea94f09
shridhar01_patil <shridhar.patil@accenture.com>
2018-07-22 23:55:53 +0100

    fixed the subnet list issue

--- eks-worker-node/eks-worker-node.tf
+++ eks-worker-node/eks-worker-node.tf
@@ -19,8 +19,8 @@ data "terraform_remote_state" "eks-cluster" {
 module "eks-worker-node" {
  source                 = "git::https://github.com/shridharMe/terraform-modules.git//modules/eks-worker-node?ref=master"
  vpc_id                 = "${data.terraform_remote_state.vpc.vpc_id}"
- public_subnet          = "${data.terraform_remote_state.vpc.public_subnets}"
- private_subnet         = "${data.terraform_remote_state.vpc.public_subnets}"
+ public_subnet          = ["${data.terraform_remote_state.vpc.public_subnets}"]
+ private_subnet         = ["${data.terraform_remote_state.vpc.public_subnets}"]
  cluster-name           = "${data.terraform_remote_state.eks-cluster.cluster-name}"
  cluster-endpoint       = "${data.terraform_remote_state.eks-cluster.cluster-endpoint}"
  cluster-security-id    = "${data.terraform_remote_state.eks-cluster.cluster-security-id}"
