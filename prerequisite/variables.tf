75a5b804dd026a6d9e9e22bb9089d54587dd38fd
shridhar01_patil <shridhar.patil@accenture.com>
2018-07-22 15:43:10 +0100

    added variables for prerequisite

---
+++ prerequisite/variables.tf
@@ -0,0 +1,17 @@
+variable "dynamodb_table" {
+  description = ""
+}
+variable "s3_region" {
+  description = ""
+}
+variable "s3_bucket_name" {
+  description = ""
+}
+
+variable "env" {
+  description = ""
+}
+
+variable "terraform_user_arn" {
+  description = ""
+}
