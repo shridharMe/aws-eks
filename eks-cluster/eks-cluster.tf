7ee9dfc2c95b0be0e563064b8333c2011cd7a2c4
shridhar01_patil <shridhar.patil@accenture.com>
2018-07-22 23:27:59 +0100

    update s3 bucker name

--- eks-cluster/eks-cluster.tf
+++ eks-cluster/eks-cluster.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config {
     bucket = "myco-terraform-state"
-    key    = "vpc/us-east-1.tfstate"
+    key    = "env:/dev-devops/vpc/us-east-1.tfstate"
     region = "us-east-1"
   }
 }
