659d87e8cd4ec29dcb816693b4ace78560280eef
shridhar01_patil <shridhar.patil@accenture.com>
2018-07-22 22:09:52 +0100

    fixed typo

--- eks-cluster/eks-cluster.tf
+++ eks-cluster/eks-cluster.tf
@@ -5,8 +5,8 @@ module "workstation-external" {
 module "eks-cluster" {
  source                     = "git::https://github.com/shridharMe/terraform-modules.git//modules/eks-cluster?ref=master"
  vpc_id                     = "${data.terraform_remote_state.vpc.vpc_id}"
- public_subnet              = "${data.terraform_remote_state.vpc.public_subnets}"
- private_subnet             = "${data.terraform_remote_state.vpc.private_subnets}"
+ public_subnets              = "${data.terraform_remote_state.vpc.public_subnets}"
+ private_subnets             = "${data.terraform_remote_state.vpc.private_subnets}"
  cluster-name               = "${var.cluster-name}" 
  workstation-external-cidr  = "${module.workstation-external.workstation-external-cidr}"
 }
