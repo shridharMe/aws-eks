12e3481236ace5da9edba4e4d3020040efd3f875
shridhar01_patil <shridhar.patil@accenture.com>
2018-07-22 23:41:30 +0100

    fixed the subnet type

--- eks-worker-node/eks-worker-node.tf
+++ eks-worker-node/eks-worker-node.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config {
     bucket = "myco-terraform-state"
-    key    = "vpc/us-east-1.tfstate"
+    key    = "env:/dev-devops/vpc/us-east-1.tfstate"
     region = "us-east-1"
   }
 }
@@ -12,7 +12,7 @@ data "terraform_remote_state" "eks-cluster" {
   backend = "s3"
   config {
     bucket = "myco-terraform-state"
-    key    = "eks-cluster/us-east-1.tfstate"
+    key    = "env:/dev-devops/eks-cluster/us-east-1.tfstate"
     region = "us-east-1"
   }
 }
