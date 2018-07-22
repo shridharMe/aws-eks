ec24f800f92b5c9120936e939179ebf1f00a7aef
shridhar01_patil <shridhar.patil@accenture.com>
2018-07-22 23:46:59 +0100

    aded s3 state config

---
+++ eks-kube-config/setup.tf
@@ -0,0 +1,20 @@
+# we expect that s3 bucket "roche-devops-terraform-dev" and dynodb table "terraformLocks" is created
+# with required policy
+
+terraform {
+  required_version = "~> 0.11.3"
+  backend "s3" {
+    bucket         = "myco-terraform-state"
+    key            = "kube-config/us-east-1.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraformLocks"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default" 
+  version = "~> 1.28"
+  version = "~> 1.0"
+}
