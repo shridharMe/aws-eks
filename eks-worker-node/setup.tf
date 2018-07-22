75e2da026a9113ffc19665a290a25fb32e14b8b0
shridhar01_patil <shridhar.patil@accenture.com>
2018-07-22 22:36:22 +0100

    add aws provider version

--- eks-worker-node/setup.tf
+++ eks-worker-node/setup.tf
@@ -11,3 +11,10 @@ terraform {
     dynamodb_table = "terraformLocks"
   }
 }
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default" 
+  version = "~> 1.28"
+  version = "~> 1.0"
+}
--- prerequisite/setup.tf
+++ prerequisite/setup.tf
@@ -1,4 +1,7 @@
+
 provider "aws" {
   region  = "us-east-1"
   profile = "default" 
+  version = "~> 1.28"
+  version = "~> 1.0"
 }
