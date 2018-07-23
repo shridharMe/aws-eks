8398615af9c3fe928b9cd07cb39320abdf52fdf7
shridhar01_patil <shridhar.patil@accenture.com>
2018-07-23 03:15:18 +0100

    removed cluster-security-id

--- eks-worker-node/eks-worker-node.tf
+++ eks-worker-node/eks-worker-node.tf
@@ -24,7 +24,7 @@ module "eks-worker-node" {
  private_subnet         = "${join(",",data.terraform_remote_state.vpc.private_subnets)}"
  cluster-name           = "${data.terraform_remote_state.eks-cluster.cluster-name}"
  cluster-endpoint       = "${data.terraform_remote_state.eks-cluster.cluster-endpoint}"
- cluster-security-id    = "${data.terraform_remote_state.eks-cluster.cluster-security-id}"
+ #cluster-security-id    = "${data.terraform_remote_state.eks-cluster.cluster-security-id}"
  node-instance-type     = "${var.node-instance-type}"
  desired-capacity       = "${var.desired-capacity}"
  max-size               = "${var.max-size}"
