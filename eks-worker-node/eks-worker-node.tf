7524549cab74657db9c83c4d46b4ecdc72994a80
shridhar01_patil <shridhar.patil@accenture.com>
2018-07-23 01:00:56 +0100

    added 2 AZ's

--- eks-worker-node/eks-worker-node.tf
+++ eks-worker-node/eks-worker-node.tf
@@ -20,8 +20,8 @@ data "terraform_remote_state" "eks-cluster" {
 module "eks-worker-node" {
  source                 = "git::https://github.com/shridharMe/terraform-modules.git//modules/eks-worker-node?ref=master"
  vpc_id                 = "${data.terraform_remote_state.vpc.vpc_id}"
- public_subnet_cidr      = "10.1.1.0/24"
- private_subnet         = "${element(data.terraform_remote_state.vpc.private_subnets, 0)}"
+ public_subnet_cidr      = "10.1.1.0/24,10.1.2.0/24"
+ private_subnet         = "${element(data.terraform_remote_state.vpc.private_subnets, 0)},${element(data.terraform_remote_state.vpc.private_subnets, 1)}"
  cluster-name           = "${data.terraform_remote_state.eks-cluster.cluster-name}"
  cluster-endpoint       = "${data.terraform_remote_state.eks-cluster.cluster-endpoint}"
  cluster-security-id    = "${data.terraform_remote_state.eks-cluster.cluster-security-id}"
